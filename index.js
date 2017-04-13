47bf019518559bd6bf3ec9d56eee7c510664d3da
Y <i_try_it@rambler.ru>
2017-04-13 14:36:42 +0300

    manage window events

--- index.js
+++ index.js
@@ -5,14 +5,29 @@ var app = electron.app;
 
 app.on('ready', function(){
     var appWindow, infoWindow;
-    appWindow = new BrowserWindow();
+    appWindow = new BrowserWindow({
+        show: false
+    });
     appWindow.loadURL('http://tut.by');
 
     infoWindow = new BrowserWindow({
         width: 400,
         height: 300,
         transparent: true,
-        frame: false
+        frame: false,
+        show: false
     });
     infoWindow.loadURL('file: //' + __dirname + '/info.html')
+
+    appWindow.once('ready-to-show', () =>{
+        appWindow.show();
+        setTimeout(()=> {
+            infoWindow.show();
+            setTimeout(()=> {
+                infoWindow.hide();
+            }, 3000)
+
+        }, 1000)
+
+    })
 })
