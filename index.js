c6c62d1c58867f3cbe4e029c0eb427a5529e24e4
Y <i_try_it@rambler.ru>
2017-04-13 14:28:23 +0300

    create transparent window

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ var BrowserWindow = electron.BrowserWindow;
 var app = electron.app;
 
 app.on('ready', function(){
-    var appWindow;
+    var appWindow, infoWindow;
     appWindow = new BrowserWindow();
     appWindow.loadURL('http://tut.by');
+
+    infoWindow = new BrowserWindow({
+        width: 400,
+        height: 300,
+        transparent: true,
+        frame: false
+    });
+    infoWindow.loadURL('file: //' + __dirname + '/info.html')
 })
